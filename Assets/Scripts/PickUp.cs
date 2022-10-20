cfbecfc9b4b532dd7328248a8cff8a1e54d7cfbe
ArgyrisPericharos <ap1747@live.mdx.ac.uk>
2022-10-20 19:26:44 +0100

    Begin on lists
    Lists for the map

---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public Transform Destination;
+
+    void OnMouseDown()
+    {
+        GetComponent<Rigidbody>().useGravity = false;
+        this.transform.position = Destination.position;
+        this.transform.parent = GameObject.Find("Destination").transform;
+    }
+
+    void OnMouseUp()
+    {
+        this.transform.parent = null;
+        GetComponent<Rigidbody>().useGravity = true;
+    }
+}
