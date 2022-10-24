9f403d4755f75d634755b094ba5334c2d76fd86a
ArgyrisPericharos <ap1747@live.mdx.ac.uk>
2022-10-24 15:13:12 +0100

    Map made
    made the map thanks to penda

---
+++ Assets/Scripts/HotSpot.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HotSpot : MonoBehaviour
+{
+    public GameObject[] Adjacent;
+    private GameObject[] Pawns;
+    private GameObject currentPawn;
+    //private List<GameObject> ListPawns;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+        for (int i = 0; i < Adjacent.Length; i++)
+        {
+            Debug.Log("Adjacent Number" + i + " is named " + Adjacent[i].name);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        for (int i = 0; i < Pawns.Length; i++)
+        {
+            if (Pawns[i].transform.position == this.transform.position)
+            {
+                getAdjacents();
+            }
+        }
+        
+
+        foreach(GameObject pawnInst in Pawns)
+        {
+
+            if (pawnInst.transform.position == this.transform.position)
+            {
+                getAdjacents();
+            }
+        }
+        */
+    }
+
+    private void OnMouseUp()
+    {
+        Debug.Log("Its a me " + gameObject.name);
+        //find current pawn
+       Pawns = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject instPawn in Pawns)
+        {
+            if (instPawn.GetComponent<PawnScript>().currentSelection)
+            {
+                currentPawn = instPawn;
+            }
+        }
+        //check if this hotspot gameobject is in the list
+        foreach (GameObject instMove in currentPawn.GetComponent<PawnScript>().availableMoves)
+        {
+            if (instMove == gameObject)
+            {
+                //then move the pawn here
+                currentPawn.transform.position = gameObject.transform.position;
+                currentPawn.GetComponent<PawnScript>().newHotspot = this;
+
+            }
+        }
+
+}
+
+    public GameObject[] getAdjacents()
+    {
+        return Adjacent;
+    }
+}
---
+++ Assets/Scripts/PawnScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PawnScript : MonoBehaviour
+{
+    public HotSpot currentHotspot;
+    public GameObject[] availableMoves;
+    public bool currentSelection;
+    public HotSpot newHotspot;
+    private GameObject[] allPawns;
+
+
+    private void Start()
+    {
+        
+    }
+    void OnMouseDown()
+    {
+
+    }
+
+    void Update()
+    {
+        // this probably doesn't go here
+        // check to see if hotspot changed?
+        //Debug.Log("What doin?");
+        if (currentHotspot != newHotspot)
+        {
+            currentHotspot = newHotspot;
+            availableMoves = currentHotspot.getAdjacents();
+        }
+        
+    }
+
+    void OnMouseUp()
+    {
+        Debug.Log("Its a me " + gameObject.name);
+        //make pawn selected 
+        // do I need tosend that somewhere?
+        SelectMe(gameObject);
+
+    }
+
+
+    void GeneratePawnList()
+    {
+        allPawns = GameObject.FindGameObjectsWithTag("Player");
+    }
+
+    void SelectMe(GameObject selectedPawn)
+    {
+
+        GeneratePawnList();
+
+        foreach(GameObject instPawn in allPawns)
+        {
+            instPawn.GetComponent<PawnScript>().currentSelection = false;
+        }
+        selectedPawn.GetComponent<PawnScript>().currentSelection = true;
+    }
+
+}
